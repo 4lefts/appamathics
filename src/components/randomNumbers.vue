ca59c3177317b4db1d12ccf8af64b9d19cb386f5
Stephen Ball <5t3ph3n.ball@gmail.com>
2017-11-05 22:04:20 +0000

    added more settings and sorting to random number generator

--- src/components/randomNumbers.vue
+++ src/components/randomNumbers.vue
@@ -19,18 +19,37 @@
         <transition name="slide">
             <div v-if="showAbout" class="about-info">
                 <h2>Random Number Generator</h2>
-                <p>Something something.</p>
+                <p>Useful for all kinds of things. Ordering, sorting, comparison, quick starter activities, practising column addition, subtraction, long or short multiplication.</p>
             </div>
         </transition>
         <transition name="slide">
             <div class="numberSettings" v-if="showSettings">
                 <h3>Settings</h3>
-                <label for="lowerBound">Lower bound:</label>
-                <input type="number" name="lowerBound" v-model.number="lowerBound">
-                <label for="upperBound">Upper bound:</label>
-                <input type="number" name="upperBound" v-model.number="upperBound">
+                <label for="lowerBound">Between...</label>
+                <input 
+                type="number"
+                name="lowerBound"
+                v-model.number="lowerBound">
+                <label for="upperBound">...and...</label>
+                <input 
+                type="number" 
+                name="upperBound" 
+                v-model.number="upperBound">
                 <label for="numberOfNumbers">Number of Numbers:</label>
-                <input type="number" name="numberOfNumbers" v-model.number="numberOfNumbers">
+                <input 
+                type="number" 
+                name="numberOfNumbers" 
+                min="1" 
+                max="10" 
+                v-model.number="numberOfNumbers">
+                <label for="decimalPlaces">Decimal Places:</label>
+                <input 
+                type="number" 
+                name="decimalPlaces"
+                min="0" 
+                max="6"
+                v-model.number="decimalPlaces">
+                <p>(Choosing {{numberOfNumbers}} numbers between {{this.bounds.lower}} and {{this.bounds.upper}}.)</p>
             </div>
         </transition>
         
@@ -46,11 +65,12 @@
 <script>
 export default {
     data: function(){
-        return { 
+        return {
             showSettings: false,
             showAbout: false,
             upperBound: 100,
             lowerBound: 20,
+            decimalPlaces: 0,
             numberOfNumbers: 3,
             randomNumbers: [],
         }
@@ -65,7 +85,7 @@ export default {
         makeNumbers: function(){
             this.randomNumbers = []
             for(let i = this.numberOfNumbers; i > 0; i--){
-                this.randomNumbers[i - 1] = ((Math.random() * (this.upperBound - this.lowerBound)) + this.lowerBound).toFixed(0)
+                this.randomNumbers[i - 1] = ((Math.random() * (this.bounds.upper - this.bounds.lower)) + this.bounds.lower).toFixed(this.decimalPlaces)
             }
         },
         go: function(){
@@ -78,6 +98,26 @@ export default {
                     t++
                 }
             }, 50)
+        }
+    },
+    computed: {
+        bounds: function(){
+            if(this.upperBound > this.lowerBound){
+                return {
+                    lower: this.lowerBound,
+                    upper: this.upperBound
+                }
+            } else if(this.upperBound < this.lowerBound){
+                return {
+                    lower: this.upperBound,
+                    upper: this.lowerBound
+                }
+            } else {
+                return {
+                    lower: this.lowerBound,
+                    upper: this.lowerBound + 1
+                }
+            }
         }
     },
     created: function(){
@@ -130,7 +170,8 @@ export default {
             font-size: 4em;
             box-sizing: border-box;
             margin: $gutter 0 0 0;
-            width: 32%;
+            padding: 0 $gutter;
+            min-width: 24%;
             height: 2em;
             @include card(1);
             text-align: center;
