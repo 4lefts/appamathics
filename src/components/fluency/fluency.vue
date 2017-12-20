07b7ffb80b2a9492bc098ca7e7f9ba887989a9da
Stephen Ball <5t3ph3n.ball@gmail.com>
2017-12-20 09:54:42 +0000

    working on fractions

--- src/components/fluency/fluency.vue
+++ src/components/fluency/fluency.vue
@@ -25,12 +25,33 @@
             <div class="sheet-container">
                 <div class="sheet-inner">
                     <div class="controls-container">
-                        <button class="reroll-button" v-on:click.prevent="reroll"><img src="../../assets/icons/refresh.svg" alt="re-generate sheet button"></button>
+                        <button class="reroll-button" v-on:click.prevent="generateQuestions"><img src="../../assets/icons/refresh.svg" alt="re-generate sheet button"></button>
                         <printButton></printButton>
                     </div>
                     <div class="sheet-output">
                         <h2>Fluency Practice</h2>
-
+                        <div v-for="(q, i) in questionData">
+                            <span>{{i + 1}}.</span>
+                            <span 
+                            v-if="q.a.hasOwnProperty('numerator')"
+                            class="frac">
+                                <span class="numerator">{{q.a.numerator}}</span>
+                                <span class="line">/</span>
+                                <span class="denominator">{{q.a.denominator}}</span>
+                            </span>
+                            <span v-else>{{q.a}}</span>
+                            <span>{{q.op}}</span>
+                            <span 
+                            v-if="q.b.hasOwnProperty('numerator')"
+                            class="frac">
+                                <span class="numerator">{{q.b.numerator}}</span>
+                                <span class="line">/</span>
+                                <span class="denominator">{{q.b.denominator}}</span>
+                            </span>
+                            <span v-else>{{q.b}}</span>
+                            <span>=</span>
+                            <span class="answer"></span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -51,13 +72,35 @@ export default {
             title: 'Fluency Sheets',
             showSettings: false,
             showAbout: false,
+            questionData: []
         }
     },
     methods: {
-        reroll: function(){
-            console.log('rerolling!')
+        generateQuestions: function(){
+            this.questionData = [
+                {
+                    a: makeProperFraction(10),
+                    b: makeProperFraction(10),
+                    op: '+'
+                },
+                {
+                    a: '1%',
+                    b: '2',
+                    frac: 'foo',
+                    op: 'of'
+                }
+            ]
         }
     }
+}
+
+function makeProperFraction(lim){
+    const denominator = Math.floor(Math.random() * (lim - 2)) + 2
+    const numerator = Math.floor(Math.random() * (denominator - 1) + 1)
+    return {
+        numerator,
+        denominator
+    } 
 }
 </script>
 
@@ -80,6 +123,32 @@ export default {
                     &:last-child{
                         margin: 0;
                     }
+                }
+            }
+            .sheet-output{
+                font-size: 2em;
+                // .answer{
+                //     display: inline-block;
+                //     width: 3em;
+                //     border-bottom: 2px solid $text;
+                // }
+                .frac{
+                    display: inline-block;
+                    position: relative;
+                    vertical-align: middle;
+                    letter-spacing: 0.001em;
+                    text-align: center;
+                }
+                .frac > span{
+                    display: block;
+                    font-size: 0.8em;
+                    padding: 0.1em;
+                }
+                .frac > span.denominator{
+                    border-top: 1.4px solid black;
+                }
+                .frac > span.line{
+                    display: none;
                 }
             }
         }
